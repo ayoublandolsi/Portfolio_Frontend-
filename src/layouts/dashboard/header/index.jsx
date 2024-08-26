df5cc74f7a7fea69d8044508386b0d9b8c78838a
ayoub <ayoublandolsie@gmail.com>
2024-08-26 20:00:43 +0100

    fix

--- src/layouts/dashboard/header/index.jsx
+++ src/layouts/dashboard/header/index.jsx
@@ -5,7 +5,7 @@ import { Box, Stack, AppBar, Toolbar, IconButton } from '@mui/material';
 // utils
 import { bgBlur } from '../../../utils/cssStyles';
 // components
-import Iconify from '../../../components/iconify';
+import Iconify from '../../../Components/iconify';
 //
 import Searchbar from './Searchbar';
 import AccountPopover from './AccountPopover';
